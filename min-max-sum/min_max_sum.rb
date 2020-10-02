dc7cf96fc7a377291f1bc6e9132346582ab94b79
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-02 16:34:18 +1000

    challenge solved

--- min-max-sum/min_max_sum.rb
+++ min-max-sum/min_max_sum.rb
@@ -1,4 +1,13 @@
-def miniMaxSum(arr)
-
-
+def mini_max_sum(arr)
+  # input arr of numbers
+  # sort the array
+  arr_sorted = arr.sort
+  # min sum by summing the four first elements
+  min_sum = arr_sorted.first(4).reduce(:+)
+  # max sum by summing the four last elements
+  max_sum = arr_sorted.last(4).reduce(:+)
+  # output string with min sum and max sum
+  puts "#{min_sum} #{max_sum}"
 end
+
+# mini_max_sum([1, 3, 5, 7, 9])
