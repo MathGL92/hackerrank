1ddfe9acbdd24859f2756dfb358c3f0b5036402a
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-03 17:58:24 +1000

    case of 12 am or 12 pm added

--- time-conversion/time_conversion.rb
+++ time-conversion/time_conversion.rb
@@ -4,8 +4,14 @@ def time_conversion(string)
   am_or_pm = string[-2..-1]
   # split the string in array of 3 elements 07, 05, 45
   array_digits = string[0..-3].split(':').map(&:to_i)
-  # if PM add 12 to first element if not don't change
-  am_or_pm == 'PM' ? array_digits[0] += 12 : array_digits[0]
+
+  # if hour == 12 AM return 00
+  if am_or_pm == 'AM' && array_digits[0] == 12
+    array_digits[0] = 0
+  # if PM && hour != 12 add 12 to first element
+  elsif am_or_pm == 'PM' && array_digits[0] != 12
+    array_digits[0] += 12
+  end
   # join with :
   array_digits.map(&:to_s).map { |digit| digit.rjust(2, '0') }.join(':')
   # output string in 24 hour format
@@ -14,3 +20,7 @@ end
 p time_conversion('07:05:45PM')
 
 # should return 19:05:45
+
+p time_conversion('12:40:22AM')
+
+# should return 00:40:22
