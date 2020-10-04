fc1ea40c97abc3f0ec06c8e480383531fc035edc
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-05 10:33:37 +1100

    starting challenge apple and orange

---
+++ apple-and-orange/apple_and_orange.rb
@@ -0,0 +1,7 @@
+def count_apples_and_oranges(start_point, end_point, apple_tree, orange_tree, apples, oranges)
+
+  # output of 2 integers on two different lines, first: number of apples on Sam's house, second: number of oranges
+end
+
+
+p count_apples_and_oranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])
