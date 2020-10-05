d5b13fc3e8e172e9ade7124d9a6c5a9a560b8f5c
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-06 09:54:07 +1100

    challenge solved

--- jumping-on-the-clouds/jumping_on_the_clouds.rb
+++ jumping-on-the-clouds/jumping_on_the_clouds.rb
@@ -1,11 +1,25 @@
 def jumping_on_clouds(clouds)
-  # input of spaced binary integers
-  # split them in array
-  # output printed integer
+  # input of bineray integers space separated clouds
+  # initialize number of steps
+  steps = 0
+  # initialize index and use while loop
+  i = 0
+  until i == clouds.size - 1
+    # if index + 2 < of clouds length && cloud index + 2 == 0, increment by 2 index and increment step by one
+    if (i + 2) < clouds.length && (clouds[i + 2]).zero?
+      i += 2
+    # else increment index by 1 and step by 1
+    else
+      i += 1
+    end
+    steps += 1
+  end
+  # output printed integer: steps
+  p steps
 end
 
-jumping_on_clouds(0 0 1 0 0 1 0)
+jumping_on_clouds([0, 0, 1, 0, 0, 1, 0])
 # should return 4
 
-# jumping_on_clouds(0 0 0 0 1 0)
+jumping_on_clouds([0, 0, 0, 0, 1, 0])
 # should return 3
