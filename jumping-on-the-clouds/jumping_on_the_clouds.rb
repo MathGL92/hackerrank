2eb575a36886607758cfb885f93b9e46a094f658
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-06 09:20:12 +1100

    starting challenge jumping on clouds

---
+++ jumping-on-the-clouds/jumping_on_the_clouds.rb
@@ -0,0 +1,11 @@
+def jumping_on_clouds(clouds)
+  # input of spaced binary integers
+  # split them in array
+  # output printed integer
+end
+
+jumping_on_clouds(0 0 1 0 0 1 0)
+# should return 4
+
+# jumping_on_clouds(0 0 0 0 1 0)
+# should return 3
