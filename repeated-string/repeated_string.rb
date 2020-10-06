6d45c39e948e9cc4d52b487920ba28a37a6b64b0
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-07 10:26:39 +1100

    challenge repeated string solved

--- repeated-string/repeated_string.rb
+++ repeated-string/repeated_string.rb
@@ -1,14 +1,18 @@
 def repeated_string(string, num)
- # input: a string to repeat and an integer (the number of characters to consider)
- # multiply the string by the num
- # split in an array
- # select the a and return the size of the array
- # output: an integer giving the number of 'a'
+  # input: a string to repeat and an integer (the number of characters to consider)
+  # number of a in string
+  number_of_a = string.count('a')
+  # number of times string fits in num
+  times = num / string.size
+  # difference between num and times
+  rest = num % string.size
+  # output: an integer giving the number of 'a'
+  # number of a's in string mutliplied by times + number of a's in slice of string
+  p number_of_a * times + string.slice(0, rest).count('a')
 end
-
 
 repeated_string('aba', 10)
 # should return 7
 
-# repeated_string('a', 1000000000000)
+repeated_string('a', 1000000000000)
 # should return 1000000000000
