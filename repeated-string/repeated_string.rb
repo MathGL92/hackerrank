6aadae816ba2c21d429f208938377521b95ca68c
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-07 09:59:59 +1100

    added pseudocode

--- repeated-string/repeated_string.rb
+++ repeated-string/repeated_string.rb
@@ -1,4 +1,14 @@
 def repeated_string(string, num)
-
-
+ # input: a string to repeat and an integer (the number of characters to consider)
+ # multiply the string by the num
+ # split in an array
+ # select the a and return the size of the array
+ # output: an integer giving the number of 'a'
 end
+
+
+repeated_string('aba', 10)
+# should return 7
+
+# repeated_string('a', 1000000000000)
+# should return 1000000000000
