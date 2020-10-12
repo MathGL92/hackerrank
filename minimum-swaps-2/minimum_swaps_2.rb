66b69930a9f1d04cbad1ab1dd4c43144b893b98b
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-12 16:10:30 +1100

    challenge minimum swaps 2 solved

--- minimum-swaps-2/minimum_swaps_2.rb
+++ minimum-swaps-2/minimum_swaps_2.rb
@@ -1,12 +1,33 @@
+require 'pry'
+
 def minimum_swaps(arr)
-  p arr
+  # input array of integers not sorted
+  # sorted array
+  sorted_arr = arr.sort
+  # initializing swapped count
+  swaps = 0
+  # until arr == sorted array do
+  until arr == sorted_arr
+    # iterate though arr
+    arr.each_with_index do |_num, index|
+      # if array[index] is greater than its supposed index + 1, swap with the element at its index and increment swap of 1
+      if arr[index] != index + 1
+        temp = arr[index]
+        arr[index] = arr[arr[index] - 1]
+        arr[temp - 1] = temp
+        swaps += 1
+      end
+    end
+  end
+  p swaps
+  # integer representing the minimum number of swaps to sort the array
 end
 
 minimum_swaps([4, 3, 1, 2])
 # should return 3
 
-# minimum_swaps([2, 3, 4, 1, 5])
+minimum_swaps([2, 3, 4, 1, 5])
 # should return 3
 
-# minimum_swaps([1, 3, 5, 2, 4, 6, 7])
+minimum_swaps([1, 3, 5, 2, 4, 6, 7])
 # should return 3
