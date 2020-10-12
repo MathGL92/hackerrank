a53cf567a3fe74b31ee2d6e53c1cd7b51c19f782
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-12 13:40:37 +1100

    starting challenge minimum swaps 2

---
+++ minimum-swaps-2/minimum_swaps_2.rb
@@ -0,0 +1,12 @@
+def minimum_swaps(arr)
+  p arr
+end
+
+minimum_swaps([4, 3, 1, 2])
+# should return 3
+
+# minimum_swaps([2, 3, 4, 1, 5])
+# should return 3
+
+# minimum_swaps([1, 3, 5, 2, 4, 6, 7])
+# should return 3
