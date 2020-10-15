725e58a5c1f6bc554761a57728d700b3ec070abd
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-16 09:03:27 +1100

    refactoring

--- new-year-chaos/new_year_chaos.rb
+++ new-year-chaos/new_year_chaos.rb
@@ -1,19 +1,16 @@
-require 'pry'
-
 def minimum_bribes(array)
   # input array not in order
-  # array ordered
-  correct_order = array.sort
+  size = array.size
   # initializing count of bribes
   count = 0
-  # guard clause: if an element is greater its original position by more than 2
-  array.each_with_index do |_elem, index|
-    return puts 'Too chaotic' if array[index] - (index + 1) > 2
-  end
+
   # until the array with bribes match exactly the array without bribes loop over it
-  until array == correct_order
+  until array == (1..size).to_a
     array.each_with_index do |_elem, index|
       # iterating through array, if elem > elem with index + 1, put item after the + 1, and increment count
+      # guard clause: if an element is greater its original position by more than 2
+      return puts 'Too chaotic' if array[index] - (index + 1) > 2
+
       if index + 1 < array.size && array[index] > array[index + 1]
         array.insert(index + 1, array.delete_at(index))
         count += 1
