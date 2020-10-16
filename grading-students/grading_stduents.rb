6ffe02769d1ec44f34a950af69b7b17e9b0a9671
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-16 12:31:38 +1100

    starting grading students challenge

---
+++ grading-students/grading_stduents.rb
@@ -0,0 +1,8 @@
+def grading_students(grades)
+    # input array of grades
+    p grades
+    # output array of grades rounded
+end
+
+grading_students([73, 67, 38, 33]);
+# should return [75, 67, 40, 33]
