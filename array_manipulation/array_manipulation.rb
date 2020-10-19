6a7e1e7483b9e31c95520159e5426809702d982f
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-20 09:14:23 +1100

    challenge started

--- array_manipulation/array_manipulation.rb
+++ array_manipulation/array_manipulation.rb
@@ -1,13 +1,18 @@
+require 'pry'
+
 def array_manipulation(n, queries)
   # input: the number n of elements in the array && queries => a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.
   # initialize array result with n * 0 elements
   final_array = Array.new(n, 0)
   # for each sub array of queries, add last element to final array of indexes between two first element
   first_array = queries.first
-  final_array.each_with_index do |elem, index|
-    if ((first_array[0] - 1)..(first_array[1] - 1]).include?(index)
-    final_array[index] += first_array[2]
+  i = first_array[0] - 1
+  while i < first_array[1]
+    final_array.each do |elem|
+      # if ((first_array[0] - 1)..(first_array[1] - 1]).include?(index)
+      final_array[i] += first_array[2]
     end
+    i += 1
   end
   # output the biggest number of the array
   p final_array
