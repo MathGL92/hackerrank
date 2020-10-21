5e99a8725795a108493906c6ae00b12b61a110d2
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-21 11:28:19 +1100

    array manipulation challenge done

--- array_manipulation/array_manipulation.rb
+++ array_manipulation/array_manipulation.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 #### brute force algorithm
 # def array_manipulation(n, queries)
 #   # input: the number n of elements in the array && queries => a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.
@@ -21,14 +23,20 @@
 
 def array_manipulation(n, queries)
   # initialize array result with (n+1) * 0 elements, because we will be updating N+1 index
-  final_array = Array.new(n+1, 0)
+  final_array = Array.new(n+2, 0)
   # for each query, just modifiy 2 values of final_array  0(1) => add k to "sums[a-1]" and susbtract k to 'sums[b]'
   queries.each do |query|
-    final_array[query[0]-1] += query[2]
-    final_array[query[1]] -= query[2]
+    final_array[query[0]] += query[2]
+    final_array[query[1]+1] -= query[2]
   end
-  p final_array
+  # prefix sum algorithm to sum final_array[index] += final_array[index-1] when index > 0 and < size-1
+  prefix_sum_algorithm = (1..final_array.size-1).map { |i| final_array[i] += final_array[i-1] }
+  # maximum element
+  p prefix_sum_algorithm.max
 end
 
-array_manipulation(10, [[1, 5, 3], [4, 8, 7], [6, 9, 1]])
+# array_manipulation(10, [[1, 5, 3], [4, 8, 7], [6, 9, 1]])
 # should return 10
+
+array_manipulation(5, [[1, 2, 100], [2, 5, 100], [3, 4, 100]])
+# should return 200
