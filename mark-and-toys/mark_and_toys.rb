437385f15235c00d81ca4b35f45791402c9460a7
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-27 08:55:59 +1100

    starting challenge mark and toys

---
+++ mark-and-toys/mark_and_toys.rb
@@ -0,0 +1,9 @@
+def maximum_toys(prices, k)
+  # input array of prices (integers) and k the amount Mark has to spend
+  # sort prices array
+  p prices.sort!
+  # while k <= price, deduct price from k
+  # output an integer representing the max number of toys Mark can buy
+end
+
+maximum_toys([1, 12, 5, 111, 200, 1000, 10], 50)
