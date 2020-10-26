cb00a8730cbcae54a6c0936e00b830983ed177c9
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-27 09:05:08 +1100

    mark and toys solved

--- mark-and-toys/mark_and_toys.rb
+++ mark-and-toys/mark_and_toys.rb
@@ -1,9 +1,16 @@
 def maximum_toys(prices, k)
   # input array of prices (integers) and k the amount Mark has to spend
   # sort prices array
-  p prices.sort!
-  # while k <= price, deduct price from k
+  prices.sort!
+  # initialize index of array to be able to loop
+  i = 0
+  # while k > price, deduct price from k and increment i by one
+  while k > prices[i] do
+    k -= prices[i]
+    i += 1
+  end
   # output an integer representing the max number of toys Mark can buy
+  p i
 end
 
 maximum_toys([1, 12, 5, 111, 200, 1000, 10], 50)
